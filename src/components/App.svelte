83d7ca36194c9d6ef11631077d2bb5402c543ada
Sukham Sidhu <ssidhu@ucsd.edu>
2024-02-15 16:20:36 -0800

    made App.svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let name = "World";
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Visit <a href="https://svelte.dev/tutorial"> Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+    }
+</style>
